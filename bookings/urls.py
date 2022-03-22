8e0a1288c774fff38a53ec89c22db7a63bd16293
saramorrin <sara.lee.morrin@googlemail.com>
2022-03-22 16:41:35 +0000

    Add context and images to homepage, create additional html files

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,4 +3,10 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('contact/', views.ContactView.as_view(), name='contact'),
+    path('mountain_lodge/', views.MountainLodgeView.as_view(), name='mountain_lodge'),
+    path('fir_pine_nest/', views.FirPineNestView.as_view(), name='fir_pine_nest'),
+    path('create_profile/', views.CreateProfileView.as_view(), name='create_profile'), 
+    path('make_reservation/', views.MakeReservationView.as_view(), name='make_reservation'), 
 ]
--- bookings/views.py
+++ bookings/views.py
@@ -9,3 +9,27 @@ from .forms import UpdateReservation, EditUserProfile
 
 class HomeView(TemplateView):
     template_name = "index.html"
+
+
+class AboutView(TemplateView):
+    template_name = "about.html"
+
+
+class ContactView(TemplateView):
+    template_name = "contact.html"
+
+
+class MountainLodgeView(TemplateView):
+    template_name = "mountain_lodge.html"
+
+
+class FirPineNestView(TemplateView):
+    template_name = "fir_pine_nest.html"
+
+
+class CreateProfileView(TemplateView):
+    template_name = "create_profile.html"
+
+
+class MakeReservationView(TemplateView):
+    template_name = "make_reservation.html"
